ea99a6ba99c0669b0c21da2e9fd4f118295c1e5e
ngdathd <ngdathd@gmail.com>
2019-10-24 18:01:25 +0700

    read json

--- app/src/main/java/com/demo/hdt/searchcategory/MainActivity.java
+++ app/src/main/java/com/demo/hdt/searchcategory/MainActivity.java
@@ -1,8 +1,17 @@
 package com.demo.hdt.searchcategory;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Writer writer = new StringWriter();
+        try (InputStream is = getResources().openRawResource(R.raw.catergory)) {
+            char[] buffer = new char[1024];
+            Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String jsonString = writer.toString();
+        Log.i("onCreate", "onCreate: " + jsonString);
     }
 }
